e50505228a05822cfe943f568f56c82d23f60f98
grant <rotartsi0482@gmail.com>
2020-03-13 16:21:51 -0700

    + fix edge cases & favicons

--- logic.js
+++ logic.js
@@ -143,269 +143,299 @@ let setTuesThursSchedule = function () {
     schedule.push([new Date(now.getTime()), offDuty]);
 };
 
-switch (now.getDay()) {
-    case (0): { // Sunday
-        // No schedule here. Special edge case.
-        break;
+let loadSchedule = function () {
+    switch (now.getDay()) {
+        case (0): { // Sunday
+            // No schedule here. Special edge case.
+            break;
+        }
+        case (1): { // Monday
+            now.setSeconds(0);
+
+            now.setHours(8);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+
+            now.setHours(8);
+            now.setMinutes(47);
+            schedule.push([new Date(now.getTime()), "Passing Period (1->2)"]);
+
+            now.setHours(8);
+            now.setMinutes(52);
+            schedule.push([new Date(now.getTime()), "Period 2"]);
+
+            now.setHours(9);
+            now.setMinutes(34);
+            schedule.push([new Date(now.getTime()), "Passing Period (2->Meeting)"]);
+
+            now.setHours(9);
+            now.setMinutes(39);
+            schedule.push([new Date(now.getTime()), "Meeting"]);
+
+            now.setHours(9);
+            now.setMinutes(59);
+            schedule.push([new Date(now.getTime()), "Break"]);
+
+            now.setHours(10);
+            now.setMinutes(10);
+            schedule.push([new Date(now.getTime()), "Passing Period (Break->3)"]);
+
+            now.setHours(10);
+            now.setMinutes(15);
+            schedule.push([new Date(now.getTime()), "Period 3"]);
+
+            now.setHours(10);
+            now.setMinutes(57);
+            schedule.push([new Date(now.getTime()), "Passing Period (3->4)"]);
+
+            now.setHours(11);
+            now.setMinutes(2);
+            schedule.push([new Date(now.getTime()), "Period 4"]);
+
+            now.setHours(11);
+            now.setMinutes(44);
+            schedule.push([new Date(now.getTime()), "L1"]);
+
+            now.setHours(12);
+            now.setMinutes(14);
+            schedule.push([new Date(now.getTime()), "L2"]);
+
+            now.setHours(12);
+            now.setMinutes(44);
+            schedule.push([new Date(now.getTime()), "Passing Period (L2->5)"]);
+
+            now.setHours(12);
+            now.setMinutes(49);
+            schedule.push([new Date(now.getTime()), "Period 5"]);
+
+            now.setHours(13);
+            now.setMinutes(31);
+            schedule.push([new Date(now.getTime()), "Passing Period (5->6)"]);
+
+            now.setHours(13);
+            now.setMinutes(36);
+            schedule.push([new Date(now.getTime()), "Period 6"]);
+
+            now.setHours(14);
+            now.setMinutes(18);
+            schedule.push([new Date(now.getTime()), "Passing Period (6->7)"]);
+
+            now.setHours(14);
+            now.setMinutes(23);
+            schedule.push([new Date(now.getTime()), "Period 7"]);
+
+            now.setHours(15);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Office Hours"]);
+
+            now.setHours(15);
+            now.setMinutes(35);
+            schedule.push([new Date(now.getTime()), offDuty]);
+
+            now.setDate(now.getDate() + 1);
+            now.setHours(8);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+
+            break;
+        }
+        case (2): { // Tuesday
+            setTuesThursSchedule();
+
+            now.setDate(now.getDate() + 1);
+            now.setHours(8);
+            now.setMinutes(0);
+            schedule.push([new Date(now.getTime()), "OH by Appt."]);
+            break;
+        }
+        case (3): { // Wednesday
+            now.setSeconds(0);
+
+            now.setHours(8);
+            now.setMinutes(0);
+            schedule.push([new Date(now.getTime()), "OH by Appt."]);
+
+            now.setHours(8);
+            now.setMinutes(25);
+            schedule.push([new Date(now.getTime()), "Passing Period (???->1)"]);
+
+            now.setHours(8);
+            now.setMinutes(30);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+
+            now.setHours(9);
+            now.setMinutes(12);
+            schedule.push([new Date(now.getTime()), "Passing Period (1->2)"]);
+
+            now.setHours(9);
+            now.setMinutes(17);
+            schedule.push([new Date(now.getTime()), "Period 2"]);
+
+            now.setHours(9);
+            now.setMinutes(59);
+            schedule.push([new Date(now.getTime()), "Break"]);
+
+            now.setHours(10);
+            now.setMinutes(10);
+            schedule.push([new Date(now.getTime()), "Passing Period (Break->3)"]);
+
+            now.setHours(10);
+            now.setMinutes(15);
+            schedule.push([new Date(now.getTime()), "Period 3"]);
+
+            now.setHours(10);
+            now.setMinutes(57);
+            schedule.push([new Date(now.getTime()), "Passing Period (3->4)"]);
+
+            now.setHours(11);
+            now.setMinutes(2);
+            schedule.push([new Date(now.getTime()), "Period 4"]);
+
+            now.setHours(11);
+            now.setMinutes(44);
+            schedule.push([new Date(now.getTime()), "L1"]);
+
+            now.setHours(12);
+            now.setMinutes(14);
+            schedule.push([new Date(now.getTime()), "L2"]);
+
+            now.setHours(12);
+            now.setMinutes(44);
+            schedule.push([new Date(now.getTime()), "Passing Period (L2->5)"]);
+
+            now.setHours(12);
+            now.setMinutes(49);
+            schedule.push([new Date(now.getTime()), "Period 5"]);
+
+            now.setHours(13);
+            now.setMinutes(31);
+            schedule.push([new Date(now.getTime()), "Passing Period (5->6)"]);
+
+            now.setHours(13);
+            now.setMinutes(36);
+            schedule.push([new Date(now.getTime()), "Period 6"]);
+
+            now.setHours(14);
+            now.setMinutes(18);
+            schedule.push([new Date(now.getTime()), "Passing Period (6->7)"]);
+
+            now.setHours(14);
+            now.setMinutes(23);
+            schedule.push([new Date(now.getTime()), "Period 7"]);
+
+            now.setHours(15);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), offDuty]);
+
+            now.setDate(now.getDate() + 1);
+            now.setHours(8);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+            break;
+        }
+        case (4): { // Thursday
+            setTuesThursSchedule();
+
+            now.setDate(now.getDate() + 1);
+            now.setHours(8);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+            break;
+        }
+        case (5): { // Friday
+            now.setSeconds(0);
+
+            now.setHours(8);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+
+            now.setHours(8);
+            now.setMinutes(46);
+            schedule.push([new Date(now.getTime()), "Passing Period (1->2)"]);
+
+            now.setHours(8);
+            now.setMinutes(51);
+            schedule.push([new Date(now.getTime()), "Period 2"]);
+
+            now.setHours(9);
+            now.setMinutes(32);
+            schedule.push([new Date(now.getTime()), "Break"]);
+
+            now.setHours(9);
+            now.setMinutes(42);
+            schedule.push([new Date(now.getTime()), "Passing Period (Break->3)"]);
+
+            now.setHours(9);
+            now.setMinutes(47);
+            schedule.push([new Date(now.getTime()), "Period 3"]);
+
+            now.setHours(10);
+            now.setMinutes(28);
+            schedule.push([new Date(now.getTime()), "Passing Period (3->4)"]);
+
+            now.setHours(10);
+            now.setMinutes(33);
+            schedule.push([new Date(now.getTime()), "Period 4"]);
+
+            now.setHours(11);
+            now.setMinutes(14);
+            schedule.push([new Date(now.getTime()), "L1"]);
+
+            now.setHours(11);
+            now.setMinutes(44);
+            schedule.push([new Date(now.getTime()), "L2"]);
+
+            now.setHours(12);
+            now.setMinutes(14);
+            schedule.push([new Date(now.getTime()), "Passing Period (L2->5)"]);
+
+            now.setHours(12);
+            now.setMinutes(19);
+            schedule.push([new Date(now.getTime()), "Period 5"]);
+
+            now.setHours(13);
+            now.setMinutes(0);
+            schedule.push([new Date(now.getTime()), "Passing Period (5->6)"]);
+
+            now.setHours(13);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 6"]);
+
+            now.setHours(13);
+            now.setMinutes(46);
+            schedule.push([new Date(now.getTime()), "Passing Period (6->7)"]);
+
+            now.setHours(13);
+            now.setMinutes(51);
+            schedule.push([new Date(now.getTime()), "Period 7"]);
+
+            now.setHours(14);
+            now.setMinutes(32);
+            schedule.push([new Date(now.getTime()), "Passing Period (7->Assembly)"]);
+
+            now.setHours(14);
+            now.setMinutes(37);
+            schedule.push([new Date(now.getTime()), "Assembly"]);
+
+            now.setHours(15);
+            now.setMinutes(35);
+            schedule.push([new Date(now.getTime()), offDuty]);
+
+            now.setDate(now.getDate() + 3);
+            now.setHours(8);
+            now.setMinutes(5);
+            schedule.push([new Date(now.getTime()), "Period 1"]);
+
+            break;
+        }
+        case (6): { // Saturday
+            // No schedule here. Special edge case.
+            break;
+        }
     }
-    case (1): { // Monday
-        now.setSeconds(0);
+};
 
-        now.setHours(8);
-        now.setMinutes(5);
-        schedule.push([new Date(now.getTime()), "Period 1"]);
-
-        now.setHours(8);
-        now.setMinutes(47);
-        schedule.push([new Date(now.getTime()), "Passing Period (1->2)"]);
-
-        now.setHours(8);
-        now.setMinutes(52);
-        schedule.push([new Date(now.getTime()), "Period 2"]);
-
-        now.setHours(9);
-        now.setMinutes(34);
-        schedule.push([new Date(now.getTime()), "Passing Period (2->Meeting)"]);
-
-        now.setHours(9);
-        now.setMinutes(39);
-        schedule.push([new Date(now.getTime()), "Meeting"]);
-
-        now.setHours(9);
-        now.setMinutes(59);
-        schedule.push([new Date(now.getTime()), "Break"]);
-
-        now.setHours(10);
-        now.setMinutes(10);
-        schedule.push([new Date(now.getTime()), "Passing Period (Break->3)"]);
-
-        now.setHours(10);
-        now.setMinutes(15);
-        schedule.push([new Date(now.getTime()), "Period 3"]);
-
-        now.setHours(10);
-        now.setMinutes(57);
-        schedule.push([new Date(now.getTime()), "Passing Period (3->4)"]);
-
-        now.setHours(11);
-        now.setMinutes(2);
-        schedule.push([new Date(now.getTime()), "Period 4"]);
-
-        now.setHours(11);
-        now.setMinutes(44);
-        schedule.push([new Date(now.getTime()), "L1"]);
-
-        now.setHours(12);
-        now.setMinutes(14);
-        schedule.push([new Date(now.getTime()), "L2"]);
-
-        now.setHours(12);
-        now.setMinutes(44);
-        schedule.push([new Date(now.getTime()), "Passing Period (L2->5)"]);
-
-        now.setHours(12);
-        now.setMinutes(49);
-        schedule.push([new Date(now.getTime()), "Period 5"]);
-
-        now.setHours(13);
-        now.setMinutes(31);
-        schedule.push([new Date(now.getTime()), "Passing Period (5->6)"]);
-
-        now.setHours(13);
-        now.setMinutes(36);
-        schedule.push([new Date(now.getTime()), "Period 6"]);
-
-        now.setHours(14);
-        now.setMinutes(18);
-        schedule.push([new Date(now.getTime()), "Passing Period (6->7)"]);
-
-        now.setHours(14);
-        now.setMinutes(23);
-        schedule.push([new Date(now.getTime()), "Period 7"]);
-
-        now.setHours(15);
-        now.setMinutes(5);
-        schedule.push([new Date(now.getTime()), "Office Hours"]);
-
-        now.setHours(15);
-        now.setMinutes(35);
-        schedule.push([new Date(now.getTime()), offDuty]);
-
-        break;
-    }
-    case (2): { // Tuesday
-        setTuesThursSchedule();
-        break;
-    }
-    case (3): { // Wednesday
-        now.setSeconds(0);
-
-        now.setHours(8);
-        now.setMinutes(0);
-        schedule.push([new Date(now.getTime()), "OH by Appt."]);
-
-        now.setHours(8);
-        now.setMinutes(25);
-        schedule.push([new Date(now.getTime()), "Passing Period (???->1)"]);
-
-        now.setHours(8);
-        now.setMinutes(30);
-        schedule.push([new Date(now.getTime()), "Period 1"]);
-
-        now.setHours(9);
-        now.setMinutes(12);
-        schedule.push([new Date(now.getTime()), "Passing Period (1->2)"]);
-
-        now.setHours(9);
-        now.setMinutes(17);
-        schedule.push([new Date(now.getTime()), "Period 2"]);
-
-        now.setHours(9);
-        now.setMinutes(59);
-        schedule.push([new Date(now.getTime()), "Break"]);
-
-        now.setHours(10);
-        now.setMinutes(10);
-        schedule.push([new Date(now.getTime()), "Passing Period (Break->3)"]);
-
-        now.setHours(10);
-        now.setMinutes(15);
-        schedule.push([new Date(now.getTime()), "Period 3"]);
-
-        now.setHours(10);
-        now.setMinutes(57);
-        schedule.push([new Date(now.getTime()), "Passing Period (3->4)"]);
-
-        now.setHours(11);
-        now.setMinutes(2);
-        schedule.push([new Date(now.getTime()), "Period 4"]);
-
-        now.setHours(11);
-        now.setMinutes(44);
-        schedule.push([new Date(now.getTime()), "L1"]);
-
-        now.setHours(12);
-        now.setMinutes(14);
-        schedule.push([new Date(now.getTime()), "L2"]);
-
-        now.setHours(12);
-        now.setMinutes(44);
-        schedule.push([new Date(now.getTime()), "Passing Period (L2->5)"]);
-
-        now.setHours(12);
-        now.setMinutes(49);
-        schedule.push([new Date(now.getTime()), "Period 5"]);
-
-        now.setHours(13);
-        now.setMinutes(31);
-        schedule.push([new Date(now.getTime()), "Passing Period (5->6)"]);
-
-        now.setHours(13);
-        now.setMinutes(36);
-        schedule.push([new Date(now.getTime()), "Period 6"]);
-
-        now.setHours(14);
-        now.setMinutes(18);
-        schedule.push([new Date(now.getTime()), "Passing Period (6->7)"]);
-
-        now.setHours(14);
-        now.setMinutes(23);
-        schedule.push([new Date(now.getTime()), "Period 7"]);
-
-        now.setHours(15);
-        now.setMinutes(5);
-        schedule.push([new Date(now.getTime()), offDuty]);
-        break;
-    }
-    case (4): { // Thursday
-        setTuesThursSchedule();
-        break;
-    }
-    case (5): { // Friday
-        now.setSeconds(0);
-
-        now.setHours(8);
-        now.setMinutes(5);
-        schedule.push([new Date(now.getTime()), "Period 1"]);
-
-        now.setHours(8);
-        now.setMinutes(46);
-        schedule.push([new Date(now.getTime()), "Passing Period (1->2)"]);
-
-        now.setHours(8);
-        now.setMinutes(51);
-        schedule.push([new Date(now.getTime()), "Period 2"]);
-
-        now.setHours(9);
-        now.setMinutes(32);
-        schedule.push([new Date(now.getTime()), "Break"]);
-
-        now.setHours(9);
-        now.setMinutes(42);
-        schedule.push([new Date(now.getTime()), "Passing Period (Break->3)"]);
-
-        now.setHours(9);
-        now.setMinutes(47);
-        schedule.push([new Date(now.getTime()), "Period 3"]);
-
-        now.setHours(10);
-        now.setMinutes(28);
-        schedule.push([new Date(now.getTime()), "Passing Period (3->4)"]);
-
-        now.setHours(10);
-        now.setMinutes(33);
-        schedule.push([new Date(now.getTime()), "Period 4"]);
-
-        now.setHours(11);
-        now.setMinutes(14);
-        schedule.push([new Date(now.getTime()), "L1"]);
-
-        now.setHours(11);
-        now.setMinutes(44);
-        schedule.push([new Date(now.getTime()), "L2"]);
-
-        now.setHours(12);
-        now.setMinutes(14);
-        schedule.push([new Date(now.getTime()), "Passing Period (L2->5)"]);
-
-        now.setHours(12);
-        now.setMinutes(19);
-        schedule.push([new Date(now.getTime()), "Period 5"]);
-
-        now.setHours(13);
-        now.setMinutes(0);
-        schedule.push([new Date(now.getTime()), "Passing Period (5->6)"]);
-
-        now.setHours(13);
-        now.setMinutes(5);
-        schedule.push([new Date(now.getTime()), "Period 6"]);
-
-        now.setHours(13);
-        now.setMinutes(46);
-        schedule.push([new Date(now.getTime()), "Passing Period (6->7)"]);
-
-        now.setHours(13);
-        now.setMinutes(51);
-        schedule.push([new Date(now.getTime()), "Period 7"]);
-
-        now.setHours(14);
-        now.setMinutes(32);
-        schedule.push([new Date(now.getTime()), "Passing Period (7->Assembly)"]);
-
-        now.setHours(14);
-        now.setMinutes(37);
-        schedule.push([new Date(now.getTime()), "Assembly"]);
-
-        now.setHours(15);
-        now.setMinutes(35);
-        schedule.push([new Date(now.getTime()), offDuty]);
-        break;
-    }
-    case (6): { // Saturday
-        // No schedule here. Special edge case.
-        break;
-    }
-}
+loadSchedule();
 
 let prevTarget;
 
@@ -413,6 +443,8 @@ let updateSchedule = function () {
     now = new Date();
     let isDone = false;
     let i = 0;
+
+    periodDisp.textContent = offDuty;
 
     schedule.forEach(function (element) {
         if (isDone) {
@@ -427,32 +459,31 @@ let updateSchedule = function () {
             if (prevTarget !== element[0]) {
                 bellSound.play();
             }
-
             prevTarget = element[0];
-            return;
+        } else {
+            i++;
         }
-
-        i++;
     });
-
     if (i > 0 && !(i === schedule.length)) {
         periodDisp.textContent = schedule[i - 1][1];
-    } else {
-        periodDisp = offDuty;
-    }
-
-    if (!isDone) {
-        if (now.getDay() === 2) {
-            schoolsOut(1, 8, 0, "OH by Appt.");
-        } else {
-            schoolsOut(1, 8, 5, "Period 1");
-        }
     }
 };
 
+now = new Date();
+let currentDay = now.getDate();
 
-setInterval(function () {
+let updateInterval;
+
+let intervalHandler = function () {
     now = new Date();
+
+    if (currentDay !== now.getDate()) {
+        schedule = [];
+        loadSchedule();
+        location.reload(); // It's a new day! We need to reload the schedule.
+        clearInterval(updateInterval);
+        updateInterval = setInterval(intervalHandler, 250);
+    }
 
     switch (now.getDay()) {
         case (0): { // Sunday
@@ -469,4 +500,6 @@ setInterval(function () {
         }
     }
 
-}, 250);
+};
+
+updateInterval = setInterval(intervalHandler, 250);
