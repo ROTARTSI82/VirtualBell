445eb373be108fd33c9a352bace2dd18651802c6
ROTARTSI82 <rotartsi0482@gmail.com>
2020-08-18 13:41:06 -0700

    Fix #9, customize-schedule.html now complete

--- scripts/customize-logic.js
+++ scripts/customize-logic.js
@@ -3,8 +3,9 @@
 let custBody = document.getElementById("schedule-custom-body");
 let dayDispBtn = document.getElementById("disp-day-btn");
 let dayDispTop = document.getElementById("disp-day");
+let dayDispBtn2 = document.getElementById("disp-day-btn2");
 
-let rowHtml = "<tr>\n" +
+let rowHtml =
     "                <th scope=\"row\"><input class=\"form-control\" placeholder=\"Event Name\" type=\"text\"></th>\n" +
     "                <td><input class=\"form-control\" placeholder=\"Start Time\" type=\"time\"></td>\n" +
     "                <td>\n" +
@@ -13,14 +14,13 @@ let rowHtml = "<tr>\n" +
     "                            <button type=\"button\" class=\"btn btn-danger\" onclick=\"this.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode.parentNode);\">Delete</button>\n" +
     "                        </div>\n" +
     "                        <div class=\"col-sm\">\n" +
-    "                            <button type=\"button\" class=\"btn btn-primary\" onclick=\"swapSiblings(this.parentNode.parentNode.parentNode.parentNode, this.parentNode.parentNode.parentNode.parentNode.previousSibling);\">Up &uparrow;</button>\n" +
+    "                            <button type=\"button\" class=\"btn btn-primary\" onclick=\"swapSiblings(this.parentNode.parentNode.parentNode.parentNode.previousSibling, this.parentNode.parentNode.parentNode.parentNode);\">Up &uparrow;</button>\n" +
     "                        </div>\n" +
     "                        <div class=\"col-sm\">\n" +
     "                            <button type=\"button\" class=\"btn btn-primary\" onclick=\"swapSiblings(this.parentNode.parentNode.parentNode.parentNode, this.parentNode.parentNode.parentNode.parentNode.nextSibling);\">Down &downarrow;</button>\n" +
     "                        </div>\n" +
     "                    </div>\n" +
-    "                </td>\n" +
-    "            </tr>";
+    "                </td>\n";
 
 let getDefaultSched = function(d) {
     switch (d) {
@@ -34,31 +34,39 @@ let getDefaultSched = function(d) {
         case 4:
             return "Passing Period (?->1)~[SA]~08~[SA]~00~[SE]~Period 1~[SA]~08~[SA]~05~[SE]~Passing Period (1->2)~[SA]~08~[SA]~47~[SE]~Period 2~[SA]~08~[SA]~52~[SE]~Break~[SA]~09~[SA]~34~[SE]~Passing Period (Break->Advisory)~[SA]~09~[SA]~47~[SE]~Advisory~[SA]~09~[SA]~52~[SE]~Passing Period (Advisory->3)~[SA]~10~[SA]~10~[SE]~Period 3~[SA]~10~[SA]~15~[SE]~Passing Period (3->4)~[SA]~10~[SA]~57~[SE]~Period 4~[SA]~11~[SA]~02~[SE]~L1~[SA]~11~[SA]~44~[SE]~L2~[SA]~12~[SA]~14~[SE]~Passing Period (L2->5)~[SA]~12~[SA]~44~[SE]~Period 5~[SA]~12~[SA]~49~[SE]~Passing Period (5->6)~[SA]~13~[SA]~31~[SE]~Period 6~[SA]~13~[SA]~36~[SE]~Passing Period (6->7)~[SA]~14~[SA]~18~[SE]~Period 7~[SA]~14~[SA]~23~[SE]~Office Hours~[SA]~15~[SA]~05~[SE]~Clubs~[SA]~15~[SA]~20~[SE]~Free Time~[SA]~15~[SA]~35";
         case 5:
-            return "Passing Period (?->1)~[SA]~08~[SA]~00~[SE]~Period 1~[SA]~08~[SA]~50~[SE]~Passing Period (1->2)~[SA]~08~[SA]~46~[SE]~Period 2~[SA]~08~[SA]~51~[SE]~Break~[SA]~09~[SA]~32~[SE]~Passing Period (Break->3)~[SA]~09~[SA]~42~[SE]~Period 4~[SA]~10~[SA]~33~[SE]~L1~[SA]~11~[SA]~14~[SE]~L2~[SA]~11~[SA]~44~[SE]~Passing Period (L2->5)~[SA]~12~[SA]~14~[SE]~Period 5~[SA]~12~[SA]~19~[SE]~Passing Period (5->6)~[SA]~13~[SA]~00~[SE]~Period 6~[SA]~13~[SA]~05~[SE]~Passing Period (6->7)~[SA]~13~[SA]~46~[SE]~Period 7~[SA]~13~[SA]~51~[SE]~Passing Period (7->Assembly)~[SA]~14~[SA]~32~[SE]~Assembly~[SA]~14~[SA]~37~[SE]~Free Time~[SA]~15~[SA]~35";
+            return "Passing Period (?->1)~[SA]~08~[SA]~00~[SE]~Period 1~[SA]~08~[SA]~05~[SE]~Passing Period (1->2)~[SA]~08~[SA]~46~[SE]~Period 2~[SA]~08~[SA]~51~[SE]~Break~[SA]~09~[SA]~32~[SE]~Passing Period (Break->3)~[SA]~09~[SA]~42~[SE]~Period 4~[SA]~10~[SA]~33~[SE]~L1~[SA]~11~[SA]~14~[SE]~L2~[SA]~11~[SA]~44~[SE]~Passing Period (L2->5)~[SA]~12~[SA]~14~[SE]~Period 5~[SA]~12~[SA]~19~[SE]~Passing Period (5->6)~[SA]~13~[SA]~00~[SE]~Period 6~[SA]~13~[SA]~05~[SE]~Passing Period (6->7)~[SA]~13~[SA]~46~[SE]~Period 7~[SA]~13~[SA]~51~[SE]~Passing Period (7->Assembly)~[SA]~14~[SA]~32~[SE]~Assembly~[SA]~14~[SA]~37~[SE]~Free Time~[SA]~15~[SA]~35";
         case 6:
             return "off_duty";
     }
 }
 
-let targetedDay = 0;
+let targetedDay = new Date().getDay();
 let nextDay = function() {
-    saveCustomSched();
+    if (!saveCustomSched(false)) {
+        return; // can't save schedule, so don't do it
+    }
+
     targetedDay++;
     if (targetedDay > 6) {
         targetedDay = 0;
     }
     dayDispBtn.innerText = getDayStr(targetedDay);
+    dayDispBtn2.innerText = getDayStr(targetedDay);
     dayDispTop.innerText = getDayStr(targetedDay);
 
     updateDisplayedSched();
 }
 let prevDay = function() {
-    saveCustomSched();
+    if (!saveCustomSched(false)) {
+        return; // can't save schedule :[
+    }
+
     targetedDay--;
     if (targetedDay < 0) {
         targetedDay = 6;
     }
     dayDispBtn.innerText = getDayStr(targetedDay);
+    dayDispBtn2.innerText = getDayStr(targetedDay);
     dayDispTop.innerText = getDayStr(targetedDay);
 
     updateDisplayedSched();
@@ -76,18 +84,26 @@ let getDayStr = function(d) {
     }
 }
 
-let saveCustomSched = function() {
+let saveCustomSched = function(b) {
     if (custBody.children.length <= 0) {
-        return;
+        localStorage.setItem(getDayStr(targetedDay) + "-schedule", "off_duty");
+        if (b) { $('#saved').modal('show'); }
+        return true;
     }
 
     let save = "";
+    let lastTime = "";
     for (let i of custBody.children) {
         if (!/^[0-9]+:[0-9]+$/g.test(i.children[1].firstElementChild.value)) {
-            let got = `"${i.children[0].firstElementChild.value}"`;
-            alert("Error: Please specify a time for all events or delete events with no time. Got " + got);
-            return;
+            $('#cantSave').modal('show');
+            return false;
         }
+
+        if (!(i.children[1].firstElementChild.value > lastTime)) {
+            $('#eventLater').modal('show');
+            return false;
+        }
+        lastTime = i.children[1].firstElementChild.value;
 
         save += i.children[0].firstElementChild.value;
         save += "~[SA]~";
@@ -103,6 +119,8 @@ let saveCustomSched = function() {
 
     console.log(save);
     localStorage.setItem(getDayStr(targetedDay) + "-schedule", save);
+    if (b) { $('#saved').modal('show'); }
+    return true;
 }
 
 let updateDisplayedSched = function() {
@@ -117,7 +135,7 @@ let updateDisplayedSched = function() {
 
         let items = sched.split("~[SE]~");
         for (let i = 0; i < items.length; i++) {
-            custBody.innerHTML += rowHtml;
+            custBody.innerHTML += "<tr>" + rowHtml + "</tr>";
         }
 
         for (let i = 0; i < items.length; i++) {
@@ -132,17 +150,30 @@ let updateDisplayedSched = function() {
     }
 }
 
-nextDay(); // Increment to Monday
+dayDispBtn.innerText = getDayStr(targetedDay);
+dayDispBtn2.innerText = getDayStr(targetedDay);
+dayDispTop.innerText = getDayStr(targetedDay);
+updateDisplayedSched();
 
-
+// Insert s2 before s1
 let swapSiblings = function(s1, s2) {
-    let prev1 = s1.previousSibling;
-    let prev2 = s2.previousSibling;
+    if (s2 === null) {
+        $('#noMovedown').modal('show');
+        return;
+    }
+    if (s1 === null) {
+        $('#noMoveup').modal('show');
+        return;
+    }
 
-    prev1.after(s2);
-    prev2.after(s1);
+
+    s1.parentNode.insertBefore(s2, s1);
 }
 
-
+let addRow = function() {
+    let nr = document.createElement("tr");
+    nr.innerHTML = rowHtml;
+    custBody.appendChild(nr);
+}
 
 
